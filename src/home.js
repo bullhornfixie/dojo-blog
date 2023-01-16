495bbbe2f81bae7e9ec27105760276b30b1b88bb
Jon Dawson <dawson.jon@hotmail.com>
2023-01-16 20:57:46 +0000

    clickEvent added

--- src/home.js
+++ src/home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log('hello, ninjas');
+  }
+
   return (
     <div classname="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
