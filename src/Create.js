9e3774b7bb9c3ba3a98d746ce7e857dd397c1ccc
Jon Dawson <dawson.jon@hotmail.com>
2023-04-25 17:13:48 +0100

    created fields for form

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('yoshi')
+
+
   return ( 
     <div className="create">
       <h2>Add New Blog</h2>
@@ -7,17 +14,28 @@ const Create = () => {
         <input
           type="text"
           required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          // when title is changed updates useState function
         />
         <label>Blog body:</label>
         <textarea
           required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
