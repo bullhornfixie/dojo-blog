52ad6ef468ac124d5cc168a4547fa26bee38e963
Jon Dawson <dawson.jon@hotmail.com>
2023-04-26 16:40:09 +0100

    new blog button

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,13 @@ const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('yoshi')
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    
+    setIsPending(true);
 
     fetch('http://localhost:8000/blogs', {
       method: 'POST',
@@ -16,6 +18,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log('new blog added');
+      setIsPending(false)
     })
   }
 
@@ -45,10 +48,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   );
